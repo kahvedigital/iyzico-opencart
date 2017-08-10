8f962ae53b2548f6f7c01877bf4f541fc33c9730
Kahve Digital <selim@kahvedigital.com>
2017-08-11 00:40:00 +0300

    Update iyzico_checkout_form.php
    Hatta gösterme kodları kaldırıldı

--- catalog/controller/extension/payment/iyzico_checkout_form.php
+++ catalog/controller/extension/payment/iyzico_checkout_form.php
@@ -1,6 +1,5 @@
 <?php
-error_reporting(1);
-ini_set("display_errors", 1);
+
 require_once DIR_SYSTEM . "library" . DIRECTORY_SEPARATOR . "iyzico" . DIRECTORY_SEPARATOR . "IyzipayBootstrap.php";
 
 class ControllerExtensionPaymentIyzicoCheckoutForm extends Controller {
