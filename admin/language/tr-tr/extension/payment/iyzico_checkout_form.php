336760b5ca6df2f8e8562e4673dca4f2bd52eb25
Kahve Digital <selim@kahvedigital.com>
2017-08-21 04:32:38 +0300

    Update iyzico_checkout_form.php

--- admin/language/tr-tr/extension/payment/iyzico_checkout_form.php
+++ admin/language/tr-tr/extension/payment/iyzico_checkout_form.php
@@ -1,22 +1,22 @@
 <?php
 
 // Heading
-$_['heading_title'] = 'Iyzico Ödeme Formu';
+$_['heading_title'] = 'iyzico Ödeme Formu';
 
 // Text 
-$_['text_link'] = 'iyzico pazar yeri sistemine kayıt olmak için';
+$_['text_link'] = 'iyzico Üye İşyeri olmak için kayıt olun';
 $_['text_payment'] = 'ödeme';
 $_['text_success'] = 'Ayarlar güncellendi';
 $_['text_iyzico_checkout_form'] = '<a href="http://www.kahvedigital.com/" target="_blank"><img src="view/image/payment/iyzico_checkout_form.png" alt="iyzico" title="iyzico"/></a>';
 $_['text_iyzico_checkout_form_info'] = "Apı Id ve Secret Key anahtarlarınızı <a href='https://merchant.iyzipay.com/settings' target='_blank'>https://merchant.iyzipay.com/settings</a> 'dan alabilirsiniz.";
 // Entry
-$_['entry_api_id_live'] = 'Live Api Id';
-$_['entry_secret_key_live'] = 'Live Secret Key';
+$_['entry_api_id_live'] = 'Canlı API ID';
+$_['entry_secret_key_live'] = 'Canlı Secret Key';
 $_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_status'] = 'Durumu';
 $_['entry_sort_order'] = 'Sıralama Türü';
 $_['entry_error_version'] = 'Yeni bir versiyon mevcut güncellemek için tıklayınız.';
-$_['entry_error_version_updated'] = 'Güncelleme Hatası! güncellemek için iyzico entegrasyon sayfasına giderek indirebilirsiniz. <a href="https://www.iyzico.com/entegrasyon/hazir-altyapi/opencart/" target="_blank">Entegrasyon sayfası</a> ';
+$_['entry_error_version_updated'] = 'Güncelleme Hatası! Manuel olarak güncellemek için <a href="https://www.iyzico.com/entegrasyon/hazir-altyapi/opencart/" target="_blank">Tıklayın</a> ';
 $_['entry_iyzico_or_text'] = 'veya iyzico entegrasyon sayfasına giderek indirebilirsiniz. <a href="https://www.iyzico.com/entegrasyon/hazir-altyapi/opencart/" target="_blank">Entegrasyon sayfası</a>';
 $_['entry_iyzico_update_button'] = 'Şimdi Güncelle';
 $_['entry_class'] = "Form Gösterimi";
@@ -28,13 +28,13 @@ $_['entry_test_tooltip'] = 'Test işlemini gerçekleştirmek için Canlı veya T
 // Error
 $_['error_permission'] = 'Bu değişikliği yapmak için yetkiniz yok!';
 $_['error_api_id_live'] = 'API ID gereklidir!';
-$_['error_secret_key_live'] = 'Gizli anahtarı gereklidir!';
+$_['error_secret_key_live'] = 'Secret Key Gereklidir!';
 $_['error_invalid_order'] = 'Geçersiz Sipariş';
 
-$_['iyzico_single_keys_must_configured'] = "Bu eklenti üzerinde değişiklik yapabilmeniz için eklendinin kurulu olması gereklidir";
+$_['iyzico_single_keys_must_configured'] = "Bu eklenti üzerinde değişiklik yapabilmeniz için eklentinin kurulu olması gereklidir";
 $_['Token Expired.'] = 'İşlem süresi doldu.'; 
 $_['Token is expired. Please try again.'] = 'İşlem süresi doldu. Lütfen tekrar deneyiniz.'; 
-$_['Invalid call.'] = 'Geçersiz arayın.';
+$_['Invalid call.'] = 'Geçersiz İstek';
 
 // Details
 $_['payment_transaction_type'] = 'Ödeme İşlemi';
@@ -56,13 +56,13 @@ $_['text_processing'] = 'İşlem Sürüyor.';
 $_['text_item_name'] = 'Ürün Adı';
 $_['text_paid_price'] = 'Tahsil Edilen Tutar';
 $_['text_total_refunded_amount'] = 'İade Edilen Tutar.';
-$_['text_action'] = 'Aksiyon';
+$_['text_action'] = 'İşlem';
 $_['text_refund'] = 'İade';
 $_['text_date_added'] = 'Eklenme Tarihi';
 $_['text_type'] = 'Tip';
 $_['text_status'] = 'Durum';
 $_['text_note'] = 'Not';
-$_['text_are_you_sure'] = 'Bu işlemi yapmak için devam etmek istiyormusunuz?';
+$_['text_are_you_sure'] = 'Bu işlemi gerçekleştirmek istediğinizden emin misiniz?';
 $_['text_please_enter_amount'] = 'Lütfen Tutar Giriniz.';
 
 $_['text_enabled'] = 'Enabled';
